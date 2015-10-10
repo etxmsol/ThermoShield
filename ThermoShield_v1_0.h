c11e2dbccd04798be22266a97873620a42350b30
Mikhail Soloviev <misha.soloviev@gmail.com>
2015-10-10 22:35:41 +0200

    SW project created

---
+++ ThermoShield_v1_0.h
@@ -0,0 +1,22 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _ThermoShield_v1_0_H_
+#define _ThermoShield_v1_0_H_
+#include "Arduino.h"
+//add your includes for the project ThermoShield_v1_0 here
+
+
+//end of add your includes here
+
+
+//add your function definitions for the project ThermoShield_v1_0 here
+
+
+
+
+//Do not add code below this line
+#endif /* _ThermoShield_v1_0_H_ */
